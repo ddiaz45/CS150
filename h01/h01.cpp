81eb71f253ba3abe460c1b2c6bc9fcd69523fab8
Diego Diaz <ddiaz45@student.cccd.edu>
2019-09-12 02:03:30 +0000

    auto commit 09/12/19 02:03:30

--- h01/h01.cpp
+++ h01/h01.cpp
@@ -8,7 +8,7 @@
 #include <iomanip>
 using namespace std;
 
-string STUDENT = "Who Are You?";  // Add your Canvas login name
+string STUDENT = "Diego Diaz";  // Add your Canvas login name
 extern string ASSIGNMENT;
 
 
@@ -18,23 +18,41 @@ extern string ASSIGNMENT;
  */
 int run()
 {
-    //  1. Title and Introduction
-  
-     // 2. Input Line: prompt and input on same line
-     cout << "  Time:";
-     cin >> timeHours;
-     cin >> timeMinutes
-     
-     cout << "  Duration:";
-     cin >> durationHours;
-     cin >> durationMinutes;
-     // 3. Processing section - Compute the results
-  // blank line
-     // 4. Output section: test data inside brackets [ ]
+    //need to get the hours and minutes for the current time. both need to be integerss
+    //hours and minutes of the duration
+    int timeHours, timeMinutes, durationHours, durationMinutes;
+    char discard;
 
-     return 0;
+    cout << "Time: ";
+    cin >> timeHours >> discard >> timeMinutes;
+
+    cout << "Duration: ";
+    cin >> durationHours >> discard >> durationMinutes;
+
 
     // Write your code here
+
+    int time = timeHours * 60 + timeMinutes;
+    int duration = durationHours * 60 + durationMinutes;
+
+    int timeafter = time + duration;
+    int timebefore = time - duration;
+
+    int afterhours = timeafter / 60;
+    int afterminutes = timeafter % 60;
+    int beforehours = timebefore / 60;
+    int beforeminutes = timebefore % 60;
+        if(beforehours == 0)
+        {
+            beforehours += 12;
+        }
+    cout << endl;
+    cout << durationHours << ":" << durationMinutes << "hours after, and before, "
+        << timeHours << ":" << timeMinutes << "is [" << afterhours << ":" << afterminutes << ", "
+        << beforehours << ":" << beforeminutes << "]" << endl;
+
+
+
 
     return 0;
 }
