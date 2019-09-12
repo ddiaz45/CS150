e1f0f30d1f157093f3fea3d7e0944c01f729ee91
Diego Diaz <ddiaz45@student.cccd.edu>
2019-09-12 01:15:52 +0000

    commiting updated starters

--- h05/h05.cpp
+++ h05/h05.cpp
@@ -1,17 +1,21 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Diego Diaz
+ *  @date 09/11/19
  *  @file h05.cpp
  */
 #include <string>
 #include <cmath>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "ddiaz45 C02418528"; // Add your Canvas/occ-email ID
 
 #include "h05.h"
 
 // Place the implementation (definition) of your function here
+
+//focal length (f)
+//thickness (d)
+//curvature r1 & r2
 
 
 
