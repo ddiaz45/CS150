1809f3893087a0bb11f85c1f85411d7deee5e9b2
Diego Diaz <ddiaz45@student.cccd.edu>
2019-09-17 10:13:18 +0000

    auto commit 09/17/19 10:13:18

--- h06/h06.cpp
+++ h06/h06.cpp
@@ -24,8 +24,8 @@ string toFrenchGender(const string& country)
     string islands = "iles";
     string vowels = "AEIOU"; //variable set that only cares about first letter
     string first = country.substr(0,1); //this grabs the first character
-    string end = country.substr(country.size()-2); // last two character
-    string last =  country.substr(country.size()-1); //last character
+    string end = country.substr(country.size() - 2); // last two character
+    string last =  country.substr(country.size() - 1); //last character
 
 
 
