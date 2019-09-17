dd7fb0076be239923480ef4745bf8a0c268d4770
Diego Diaz <ddiaz45@student.cccd.edu>
2019-09-17 21:36:05 +0000

    auto commit 09/17/19 21:36:05

--- h07/h07.cpp
+++ h07/h07.cpp
@@ -1,17 +1,27 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Diego Diaz
+ *  @date 09/17/2019
  *  @file h07.cpp
  */
 #include <string>
 #include <cctype>
 using namespace std;
 
-string STUDENT = "WHO AM I?";  // Add your Canvas login name
+string STUDENT = "ddiaz45";  // Add your Canvas login name
 
 // Implement your function here
+//step 1 write the skeleton
+int sumNums(const string& sN)
+{
+    //input is string and output is a integer
+    int total = 0;
 
 
+
+
+    //return statement at the end
+    return total;
+}
 
 /////////////// Optional Student Code /////////////////
 int run()
