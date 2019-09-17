bd329283bf4ab68c1c23a33b3cf67fef662fd6d2
Diego Diaz <ddiaz45@student.cccd.edu>
2019-09-17 08:41:28 +0000

    auto commit 09/17/19 08:41:28

--- h06/h06.cpp
+++ h06/h06.cpp
@@ -13,8 +13,29 @@ string STUDENT = "Diego Diaz";  // Add your Canvas login name
 //defining the function
 string toFrenchGender(const string& country)
 {
+    //result is initialized
     string result;
+    string prefix;
+    //result is given a value
     result = "who knows";
+    //returns the variable
+
+    //country end in letter e or o? -feminine prefix -> la
+    //otherwise- masculine prefix -> le
+    //the exceptions
+        //belize cambodge mexique mozambique zaire zimbabwe -> le
+        //starts with a vowel? -> prefix l'
+        //plural -> prefix les
+        // israel, madasgascar, sri lanka, singapore, monaco, cuba, and cyprus
+        //these have no prefix
+
+    //need to write if statements
+    //create a string variable, prefix inside processing
+
+
+
+    //then make result add prefix and country
+    result = prefix + country;
     return result;
 }
 
