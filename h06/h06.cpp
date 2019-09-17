63f949f0db4a4657f88e240f2932abb8057b793f
Diego Diaz <ddiaz45@student.cccd.edu>
2019-09-17 10:36:19 +0000

    auto commit 09/17/19 10:36:19

--- h06/h06.cpp
+++ h06/h06.cpp
@@ -29,7 +29,8 @@ string toFrenchGender(const string& country)
 
 
     if (masculine == "Belize" || masculine == "Cambodge" || masculine == "Honduras" || masculine == "Mexique" ||
-        masculine == "Mozambique" || masculine == "Costa Rica" || masculine == "Zimbabwe" )
+        masculine == "Mozambique" || masculine == "Costa Rica" || masculine == "Zimbabwe" ||
+        masculine == "Canada")
     {
         prefix = "le ";
     }
