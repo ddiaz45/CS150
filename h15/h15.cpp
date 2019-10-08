c3de8c20909958c24e8eba7c9bee62368b58c0e2
Diego Diaz <ddiaz45@student.cccd.edu>
2019-10-08 20:25:48 +0000

    auto commit 10/08/19 20:25:47

--- h15/h15.cpp
+++ h15/h15.cpp
@@ -1,12 +1,12 @@
 /**
- *  @author Put your name here
- *  @date Put the date here
+ *  @author Diego Diaz
+ *  @date 10/8/2019
  *  @file h15.cpp
  */
 #include <string>
 using namespace std;
 
-string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+string STUDENT = "Diego Diaz"; // Add your Canvas/occ-email ID
 
 // NO OTHER CODE IN THIS FILE
 // PUT EVERYTHING IN THE HEADER FILE
