13a178d757be6e0f0bd82dcf90433cf4c7ed2b13
Diego Diaz <ddiaz45@student.cccd.edu>
2019-10-16 00:00:31 +0000

    auto commit 10/16/19 00:00:29

--- h16/h16.cpp
+++ h16/h16.cpp
@@ -12,8 +12,43 @@ string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
 #include "h16.h"
 // Add your implementation here
 
+//istringstream out;
 
+void get(istream& in, Point& p)
+{
+    char ch;
+    in >> p.x >> ch >> p.y;
+}
 
+void print( ostream& out, const Point& p)
+{
+    out << "(" << p.x << ", " << p.y << ")";
+}
+
+double distanceBetween(const Point& a, const Point& b)
+{
+    int distx = a.x - b.y;
+
+    int disty = a.y - a.x;
+
+    return sqrt(distx * disty + disty * disty);
+}
+Point midpoint(const Point& a, const Point& b)
+{
+    Point mid;
+    mid.x = ((a.x + b.x) / 2);
+    mid.y = ((a.y + b.y) / 2);
+    return mid;
+}
+
+double perimeter(const Triangle & t)
+{
+    double perimter = 0.0;
+    perimter += distanceBetween(t.p1, t.p2);
+    perimter += distanceBetween(t.p2, t.p3);
+    perimter += distanceBetween(t.p3, t.p1);
+    return perimter;
+}
 /////////////// STUDENT TESTING ////////////////////
 int run()
 {
